d9d62d3623b3e69b1cb171ae6e84b4cc0237e148
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-04-26 17:55:06 +0530

    Navbar made responsive

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
-
+import { useState } from "react";
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <nav className="bg-gray-800">
+      {/* <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
@@ -27,6 +32,85 @@ const NavBar = () => {
                 </Link>
               </div>
             </div>
+          </div>
+        </div>
+      </nav> */}
+      <nav className="bg-gray-800">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Link href="/">
+                  <span className="text-white font-bold text-xl cursor-pointer">
+                    Paper Magic Supplies
+                  </span>
+                </Link>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center">
+                <Link href="/privacy-policy">
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                    Privacy Policy
+                  </span>
+                </Link>
+                <Link href="/delete-info">
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                    Delete Info
+                  </span>
+                </Link>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                type="button"
+                onClick={toggleMenu}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+                aria-label="Main menu"
+                aria-expanded="false"
+              >
+                <svg
+                  className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                <svg
+                  className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={`${isOpen ? "block" : "hidden"} md:hidden bg-gray-800`}>
+          <div className="px-2 pt-2 pb-3 sm:px-3">
+            <Link href="/privacy-policy">
+              <span className="block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                Privacy Policy
+              </span>
+            </Link>
+            <Link href="/delete-info">
+              <span className="block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                Delete Info
+              </span>
+            </Link>
           </div>
         </div>
       </nav>
